1001542a8fad38c9b5c5c9b2d436d0c34d7ced72
SPachari <shankar.pachari@gmail.com>
2019-12-08 13:16:59 -0800

    basic state refresh

--- src/index.js
+++ src/index.js
@@ -3,14 +3,29 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./display";
 
 class App extends React.Component {
-  render() {
-    //Get the user's browser location
+  //A generic Javascript meethod. similar to Java constructor.
+  //props mandatory.
+  constructor(props) {
+    //similar to Java super(). Calls the base class constructor.
+    super(props);
+    //init state
+    this.state = { lat: null };
+
+    //Two callbacks - first for success, second in case of failure
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => {
+        console.log(position.coords.latitude);
+        this.setState({ lat: position.coords.latitude });
+      },
       err => console.log(err)
     );
+  }
 
-    return <div>Latitude:</div>;
+  //Default mandatory method for every react class based component.
+  render() {
+    //Get the user's browser location
+
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
