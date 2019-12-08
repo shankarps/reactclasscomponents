c496ad51949a9b242c5318eff9e85d0c0baee3ff
SPachari <shankar.pachari@gmail.com>
2019-12-08 12:45:04 -0800

    basic comonent added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./display";
 
-const App = () => {
-  //Get the user's browser location
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div>Hi there</div>;
-};
+class App extends React.Component {
+  render() {
+    //Get the user's browser location
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+
+    return <div>Latitude:</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
